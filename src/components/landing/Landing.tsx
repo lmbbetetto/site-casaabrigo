ae33633ac4120e5dbbfc1deec86a4c17e6deb88c
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-06-14 22:05:25 -0300

    commit

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -2,8 +2,13 @@ import './landing.css'
 
 function Landing(){
     return(
-        <div className="container nome__cabecalho">
+        <div className="nome__cabecalho">
             <h1>ACIPP Casa Abrigo</h1>
+
+            <div className="quote-externo">
+                <p>"Amar é acolher, é compreender, é fazer o outro crescer."</p>
+            </div>
+
         </div>
     )
 }
