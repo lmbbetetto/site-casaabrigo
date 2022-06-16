3143bcafc8636c62c51c6d0fd08cccab47ab729a
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-06-16 00:44:27 -0300

    Commit att

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 
 import './components/header/header.css'
 import logo from './assets/logo.png'
@@ -9,35 +9,35 @@ import './components/footer/footer.css'
 import {ImFacebook} from 'react-icons/im'
 
 import Landing from './components/landing/Landing'
+import Contato from './components/contact/Contact'
 
 
 function App() {
 
   return (
-    <Router>
+    <>
       <header>
         <div className='container'>
           <nav className='container__header header_menu'>
           <ul>
-            <input
-              type="search"
-              className='search'
-              placeholder='Procurando algo?'
-            />
+            <li><Link to='/'>Home</Link></li>
             <li><a href="">Sobre</a></li>
             <li><a href="">Transparência</a></li>
-            <li><a href="">Contato</a></li>
+            <li><Link to="/contato">Contato</Link></li>
             <a href=""><button className='btn btn-yellow'>Doação</button></a>
           </ul>
           </nav>
         </div>
 
         <div className='logo__casa'>
-            <img src={logo} alt="logo casalar" />
+            <img src={logo} alt="logo casalar"/>
         </div>
       </header>
 
-      <Route component = { Landing } path='/' />
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+        <Route path='/contato' element={<Contato/>} />
+      </Routes>
 
       <div className="wave">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffff" fill-opacity="1" d="M0,256L48,224C96,192,192,128,288,138.7C384,149,480,235,576,234.7C672,235,768,149,864,133.3C960,117,1056,171,1152,186.7C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
@@ -47,10 +47,10 @@ function App() {
         <p className='footer__logo'>ACIPP Casa Abrigo</p>
   
         <ul className='permalinks footer'>
-          <li><a href="#">Home</a></li>
+          <li><Link to='/'>Home</Link></li>
           <li><a href="#about">Sobre</a></li>
           <li><a href="#experience">Transparência</a></li>
-          <li><a href="#contact">Contato</a></li>
+          <li><Link to='/contato'>Contato</Link></li>
         </ul>
   
         <div className="footer__socials">
@@ -62,7 +62,7 @@ function App() {
         </div>
       </footer>
 
-    </Router>
+    </>
   )
 }
 
---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,65 @@
+import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { BsWhatsapp } from 'react-icons/bs'
+import { IoLogoFacebook } from 'react-icons/io'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
+type ContactType = {
+  onSubmitContact: (e: any) => void
+}
+
+
+function Contact({ onSubmitContact }: ContactType) {
+  const form = useRef<any>();
+
+  //Alterar Any
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    //Tipar form.current
+    emailjs.sendForm('service_98n5t52', 'template_10jwbbz', form.current, 'EHQntXXmTW_cNvFQC')
+
+    e.target.reset()
+  };
+  return (
+    <section id='contact' className='contact'>
+      <h5>Entre em contato</h5>
+      <h2>Contate-me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>acippcasabrigo@hotmail.com</h5>
+            <a href="mailto:acippcasabrigo@hotmail.com" target='_blank'>Envie uma mensagem</a>
+          </article>
+
+          <article className="contact__option">
+            <IoLogoFacebook className='contact__option-icon' />
+            <h4>Facebook</h4>
+            <h5>ACIPP Casa Abrigo</h5>
+            <a href="https://www.facebook.com/acippcasaabrigo/" target='_blank'>Envie uma mensagem</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+55 18 99656-3354</h5>
+            <a href="https://wa.me/5518996563354" target='_blank'>Envie uma mensagem</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Nome completo' required />
+          <input type="email" name="email" placeholder='Email' required />
+          <textarea name="message" rows={7} placeholder='Mensagem' required></textarea>
+          <button type='submit' onClick={onSubmitContact} className='btn btn-yellow'>Enviar</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
