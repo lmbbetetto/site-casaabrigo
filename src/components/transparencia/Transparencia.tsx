21b7300b183cdd65d1c7d01a404e806a7103e07f
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-06-22 13:28:17 -0300

    commit

--- src/components/transparencia/Transparencia.tsx
+++ src/components/transparencia/Transparencia.tsx
@@ -1,8 +1,53 @@
 import './transparencia.css'
 
+import {MdPictureAsPdf} from 'react-icons/md'
+
 function Transparencia(){
     return(
-        <h5>transparencia</h5>
+        <section id='transparencia'>
+        <div className="inicio">
+          <h2>Como ajudar a Casa Abrigo?</h2>
+          <h5>Você pode ajudar a ACIPP Casa Abrigo de várias maneiras:</h5>
+        </div>
+
+        <div className="container transparencia__container">
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3>Nome Item</h3>
+          <a className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3>Nome Item</h3>
+          <a className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3>Nome Item</h3>
+          <a className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3>Nome Item</h3>
+          <a className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3>Nome Item</h3>
+          <a className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3>Nome Item</h3>
+          <a className="btn-transparencia">Download</a>
+        </article>
+        </div>
+    </section>
     )
 }
 
