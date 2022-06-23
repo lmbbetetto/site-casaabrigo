f8efe0b583aa4ea62d466e7a1cc72f6d5c473858
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-06-23 20:30:13 -0300

    commit

--- src/components/transparencia/Transparencia.tsx
+++ src/components/transparencia/Transparencia.tsx
@@ -9,6 +9,21 @@ import atividades from '../../assets/transparencia/RELATORIO_ANUAL_DE_ATIVIDADES
 import plano06 from '../../assets/transparencia/Plano_de_Trabalho_e_Proposta-0006-2022.pdf'
 import plano07 from '../../assets/transparencia/Plano_de_Trabalho_e_Proposta-0007-2022.pdf'
 import plano08 from '../../assets/transparencia/Plano_de_Trabalho_e_Proposta-0008-2022.pdf'
+import sebas from '../../assets/transparencia/CEBAS_2025.pdf'
+import sebas1 from '../../assets/transparencia/CEBAS_2025_1.pdf'
+import cnpj from '../../assets/transparencia/CNPJ.pdf'
+import crce from '../../assets/transparencia/CRCE.pdf'
+import estatuto from '../../assets/transparencia/ESTATUTO_2018.pdf'
+import insc from '../../assets/transparencia/INSC_CMAS.pdf'
+import cmdca from '../../assets/transparencia/INSC_CMDCA.pdf'
+import emenda_impositiva from '../../assets/transparencia/PL_2021_EMENDAS_IMPOSITIVAS.pdf'
+import pt_2021 from '../../assets/transparencia/PT _2021_FEDERAL.pdf'
+import municipal from '../../assets/transparencia/TA_06_2021_MUNICIPAL.pdf'
+import estadual from '../../assets/transparencia/TA_07_2021_ESTADUAL.pdf'
+import ultilidade from '../../assets/transparencia/utilidade_publica_municipal.pdf'
+
+
+
 
 function Transparencia(){
     return(
@@ -61,6 +76,78 @@ function Transparencia(){
           <MdPictureAsPdf className='about__icon' />
           <h3 className='transparencia_h3'>Plano de Trabalho e Proposta - 0008/2022</h3>
           <a  href={plano08} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>CEBAS - 2025</h3>
+          <a  href={sebas} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>CEBAS - 2025</h3>
+          <a  href={sebas1} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>CNPJ</h3>
+          <a  href={cnpj} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>CRCE</h3>
+          <a  href={crce} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>Estatuto - 2018</h3>
+          <a  href={estatuto} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>INSC - CMAS</h3>
+          <a  href={insc} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>INSC - CMDCA</h3>
+          <a  href={cmdca} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>Emendas Impositivas</h3>
+          <a  href={emenda_impositiva} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>PT - 2021 - Federal</h3>
+          <a  href={pt_2021} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>TA - 06/2021 - Municipal</h3>
+          <a  href={municipal} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>TA - 07/2021 - Estadual</h3>
+          <a  href={estadual} download className="btn-transparencia">Download</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <MdPictureAsPdf className='about__icon' />
+          <h3 className='transparencia_h3'>Ultilidade Publica Municipal</h3>
+          <a  href={ultilidade} download className="btn-transparencia">Download</a>
         </article>
         </div>
     </section>
