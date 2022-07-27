10eb424485f8f213061ac3fd3894e90420711b68
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-07-26 21:49:19 -0300

    commit landing

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,23 +1,37 @@
-import { Link } from 'react-router-dom'
-import './landing.css'
+import { Link } from "react-router-dom";
+import "./landing.css";
 
-function Landing(){
-    return(
-        <>
-        <div className="nome__cabecalho">
-            <h1>ACIPP Casa Abrigo</h1>
+import { ImSearch } from "react-icons/im";
+import { BsCashCoin } from "react-icons/bs";
 
-            <div className="quote-externo">
-                <p>"Amar é acolher, é compreender, é fazer o outro crescer."</p>
-            </div>
+function Landing() {
+  return (
+    <>
+      <div className="bg"></div>
 
-            <div className="conheca__mais">
-                <p>Conheça mais sobre a associação</p>
-                <Link to="/sobre"><button className="btn btn-yellow">Sobre</button></Link>
-            </div>
+      <div className="bg-apoie">
+        <div className="container apoie-cit">
+          <div className="botao-apoie">
+            <ImSearch className="icon-landing" />
+            <h3>
+              Conheça a nossa história, citações de nossa sócia fundadora e
+              voluntários!
+            </h3>
+            <button className="btn-transparencia">Conheça a Casa Abrigo</button>
+          </div>
+          <div className="botao-apoie">
+            <BsCashCoin className="icon-landing" />
+            <h3>Descubra as várias maneiras de ajudar a ACIP Casa Abrigo!</h3>
+            <button className="btn-transparencia">Apoie</button>
+          </div>
         </div>
-        </>
-    )
+      </div>
+
+      <div>
+        
+      </div>
+    </>
+  );
 }
 
-export default Landing
+export default Landing;
