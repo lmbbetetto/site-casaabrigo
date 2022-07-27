a82fba55a6f030e7b135a0d69acd0f0655dcdedb
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-07-26 22:22:50 -0300

    modall add

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,51 @@
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes, Link } from "react-router-dom";
 
-import './components/wave/wave.css'
+import "./components/wave/wave.css";
 
-import './components/footer/footer.css'
-import { ImFacebook } from 'react-icons/im'
+import "./components/footer/footer.css";
+import { ImFacebook } from "react-icons/im";
 
-import Landing from './components/landing/Landing'
-import Contato from './components/contact/Contact'
-import Sobre from './components/about/About'
-import Transparencia from './components/transparencia/Transparencia'
-import Doe from './components/doe/Doe'
-import Header from './components/header/Header'
-import { Container } from './homeStyles'
-
+import Landing from "./components/landing/Landing";
+import Contato from "./components/contact/Contact";
+import Sobre from "./components/about/About";
+import Transparencia from "./components/transparencia/Transparencia";
+import Doe from "./components/doe/Doe";
+import Header from "./components/header/Header";
+import { Container } from "./homeStyles";
+import Footer from "./components/footer/Footer";
+import Wave from "./components/wave/Wave";
+import { useState } from "react";
+import SubmitModal from "./components/Modal/modal";
 
 function App() {
+  const [onSubmitContact, setOnSubmitContact] = useState(false);
+
+  function handleOpenModal() {
+    setOnSubmitContact(true);
+    console.log(onSubmitContact);
+  }
+
+  function handleCloseModal() {
+    setOnSubmitContact(false);
+  }
+
   return (
     <Container>
       <Header />
 
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/contato' element={<Contato onSubmitContact={function (e: any): void {
-          throw new Error('Function not implemented.')
-        }} />} />
-        <Route path='/sobre' element={<Sobre />} />
-        <Route path='/transparencia' element={<Transparencia />} />
-        <Route path='/ajude' element={<Doe />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/contato" element={<Contato onSubmitContact={handleOpenModal} />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/transparencia" element={<Transparencia />} />
+        <Route path="/ajude" element={<Doe />} />
       </Routes>
 
-      <div className="wave">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#cf57b2" fill-opacity="1" d="M0,256L48,224C96,192,192,128,288,138.7C384,149,480,235,576,234.7C672,235,768,149,864,133.3C960,117,1056,171,1152,186.7C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-      </div>
-
-      <footer>
-        <p className='footer__logo'>ACIPP Casa Abrigo</p>
-
-        <ul className='permalinks footer'>
-          <li><Link to='/'>Início</Link></li>
-          <li><Link to='/sobre'>Sobre</Link></li>
-          <li><Link to='/transparencia'>Transparência</Link></li>
-          <li><Link to='/contato'>Contato</Link></li>
-        </ul>
-
-        <div className="footer__socials">
-          <a href="https://www.facebook.com/acippcasaabrigo" target='_black'><ImFacebook /></a>
-        </div>
-
-        <div className="footer__copyright">
-          <small>&copy; ACIPP Casa Abrigo | Todos os direitos reservados.</small>
-        </div>
-
-        <div className="footer__copyright">
-          <small>Criado por <a href='https://www.leonardobetetto.dev' target='_blank'>Leonardo Betetto</a></small>
-        </div>
-      </footer>
-
+      <SubmitModal isOpen={onSubmitContact} onRequestClose={handleCloseModal} />
+      <Wave />
+      <Footer />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,27 +1,47 @@
-import './footer.css'
-import {ImFacebook} from 'react-icons/im'
+import "./footer.css";
+import { ImFacebook } from "react-icons/im";
+import { Link } from "react-router-dom";
 
-function Footer(){
-    return(
-        <footer>
-        <p className='footer__logo'>ACIPP Casa Abrigo</p>
-  
-        <ul className='permalinks footer'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#about">Sobre</a></li>
-          <li><a href="#experience">Transparência</a></li>
-          <li><a href="#contact">Contato</a></li>
-        </ul>
-  
-        <div className="footer__socials">
-          <a href="https://www.facebook.com/acippcasaabrigo" target='_black'><ImFacebook/></a>
-        </div>
-  
-        <div className="footer__copyright">
-          <small>&copy; ACIPP Casa Abrigo. Todos os direitos reservados.</small>
-        </div>
-      </footer>
-    )
+function Footer() {
+  return (
+    <footer>
+      <p className="footer__logo">ACIPP Casa Abrigo</p>
+
+      <ul className="permalinks footer">
+        <li>
+          <Link to="/">Início</Link>
+        </li>
+        <li>
+          <Link to="/sobre">Sobre</Link>
+        </li>
+        <li>
+          <Link to="/transparencia">Transparência</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/acippcasaabrigo" target="_black">
+          <ImFacebook />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; ACIPP Casa Abrigo | Todos os direitos reservados.</small>
+      </div>
+
+      <div className="footer__copyright">
+        <small>
+          Criado por{" "}
+          <a href="https://www.leonardobetetto.dev" target="_blank">
+            Leonardo Betetto
+          </a>
+        </small>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
