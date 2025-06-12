d8d0f37a1ffaf587fe7ecd1543f1702206f513a6
Leonardo <leonardo.batista@startyouapp.com.br>
2025-06-12 11:30:00 -0300

    Feature: new documents 2024

--- src/pages/transparencia/mock/mock-2024.ts
+++ src/pages/transparencia/mock/mock-2024.ts
@@ -8,62 +8,186 @@ import PDF7 from "../../../assets/transparenciaaa/2024/TA_50_23_AO_TF_13_23_ACIP
 import PDF8 from "../../../assets/transparenciaaa/2024/Termo_0004244_2024.07.16_TF_00062024_PP_02802024_ACIPP_EME_M_R__56.000_00_assinado.pdf";
 import PDF9 from "../../../assets/transparenciaaa/2024/Termo_0004764_2024.07.22_TF_00092024_ACIPP_FMDCA_Prop_294_assinado.pdf";
 
+import PDF10 from "../../../assets/transparenciaaa/2024/novos/ATA2025.2026.pdf";
+import PDF11 from "../../../assets/transparenciaaa/2024/novos/ESTADUAL RP 10 TF 13 2022 TA 05 24 E TA 62 24.pdf";
+import PDF12 from "../../../assets/transparenciaaa/2024/novos/ESTADUAL TF 132022 TA 5 2024 DESPESAS ANUAL 2024.crdownload.pdf";
+import PDF13 from "../../../assets/transparenciaaa/2024/novos/ESTATUTO 2018-CASA ABRIGO.pdf";
+import PDF14 from "../../../assets/transparenciaaa/2024/novos/FEDERAL DEMONSTRATIVO REND TF 11 2022 TA 6 24.pdf";
+import PDF16 from "../../../assets/transparenciaaa/2024/novos/FEDERAL REL EXECUÇÃO FINANCEIRA TF 11 2022 TA 6 24.pdf";
+import PDF17 from "../../../assets/transparenciaaa/2024/novos/FEDERAL RELAÇÃO DESPESAS TF 11 2022 TA 6 24.pdf";
+import PDF18 from "../../../assets/transparenciaaa/2024/novos/FEDERAL RP 10 TF 11 2022 TA 6 24.pdf";
+import PDF19 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL EMENDAS IMPOSITIVAS TF 6 2024 DESPESAS ANUAL 2024.crdownload.pdf";
+import PDF20 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL FMDCA TF 13 2023 TA 50 2023 DESPESAS ANUAL 2024.crdownload.pdf";
+import PDF21 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL FMDCA TF 9 2024 DESPESAS ANUAL 2024.crdownload.pdf";
+import PDF22 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL RP 10 TF 06 2024.pdf";
+import PDF23 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL RP 10 TF 09 2024.pdf";
+import PDF24 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL RP 10 TF 12 2022 TA 03 24.pdf";
+import PDF25 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL TF 12 2022 TA 3 2024 DESPESAS ANUAL 2024.crdownload.pdf";
+
 export const documents2024 = [
   {
-    name: "Documentos",
+    name: "Balanço - Balancete 2024",
     documents: [
       {
-        title:
-          "Balanço - Balancete 2024",
+        title: "Balanço - Balancete 2024",
         link: PDF1,
         newTab: true,
       },
+    ],
+  },
+  {
+    name: "Ata 2025 - 2026",
+    documents: [
       {
-        title:
-          "Estadual - TA 05/24 ao TF 13/22 - ACIPP",
+        title: "Ata 2025 - 2026",
+        link: PDF10,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "Estatuto 2018 – Casa Abrigo",
+    documents: [
+      {
+        title: "Estatuto 2018 – Casa Abrigo",
+        link: PDF13,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "TF 13/2022 - TA 05/2024",
+    documents: [
+      {
+        title: "Estadual - TA 05/24 ao TF 13/22 - ACIPP",
         link: PDF2,
         newTab: true,
       },
       {
-        title:
-          "Federal - TA 06/24 ao TF 11/22 - ACIPP",
+        title: "Estadual RP 10 – TF 13/2022 – TA 05/24 e TA 62/24",
+        link: PDF11,
+        newTab: true,
+      },
+      {
+        title: "Estadual TF 13/2022 – TA 5/2024 – Despesas Anual 2024",
+        link: PDF12,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "TF 11/2022 - TA 06/2024",
+    documents: [
+      {
+        title: "Federal - TA 06/24 ao TF 11/22 - ACIPP",
         link: PDF3,
         newTab: true,
       },
       {
+        title: "Federal – Demonstrativo de Rendimento TF 11/2022 – TA 6/24",
+        link: PDF14,
+        newTab: true,
+      },
+      {
         title:
-          "Municipal - TA 03/24 ao TF 12/22 - ACIPP",
+          "Federal – Relatório de Execução Financeira TF 11/2022 – TA 6/24",
+        link: PDF16,
+        newTab: true,
+      },
+      {
+        title: "Federal – Relação de Despesas TF 11/2022 – TA 6/24",
+        link: PDF17,
+        newTab: true,
+      },
+      {
+        title: "Federal RP 10 – TF 11/2022 – TA 6/24",
+        link: PDF18,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "TF 12/2022 - TA 03/2024",
+    documents: [
+      {
+        title: "Municipal - TA 03/24 ao TF 12/22 - ACIPP",
         link: PDF4,
         newTab: true,
       },
       {
-        title:
-          "Plano - 16/07/2024 - TF 0006/2024 - PP 0280/2024",
+        title: "Municipal RP 10 – TF 12/2022 – TA 03/24",
+        link: PDF24,
+        newTab: true,
+      },
+      {
+        title: "Municipal TF 12/2022 – TA 3/2024 – Despesas Anual 2024",
+        link: PDF25,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "TF 06/2024",
+    documents: [
+      {
+        title: "Plano - 16/07/2024 - TF 06/2024 - PP 0280/2024",
         link: PDF5,
         newTab: true,
       },
       {
-        title:
-          "Plano - 22/07/2024 - TF 0009/2024 - ACIPP FMDCA",
-        link: PDF6,
-        newTab: true,
-      },
-      {
-        title:
-          "TA 50/23 ao TF 13/23 - ACIPP",
-        link: PDF7,
-        newTab: true,
-      },
-      {
-        title:
-          "Termo - 16/07/2024 - TF 0006/2024 - PP 0280/2024",
+        title: "Termo - 16/07/2024 - TF 06/2024 - PP 0280/2024",
         link: PDF8,
         newTab: true,
       },
       {
         title:
-          "Termo - 22/07/2024 - TF 0009/2024 - ACIPP FMDCA",
+          "Municipal – Emendas Impositivas TF 6/2024 – Despesas Anual 2024",
+        link: PDF19,
+        newTab: true,
+      },
+      {
+        title: "Municipal RP 10 – TF 06/2024",
+        link: PDF22,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "TF 09/2024",
+    documents: [
+      {
+        title: "Plano - 22/07/2024 - TF 09/2024 - ACIPP FMDCA",
+        link: PDF6,
+        newTab: true,
+      },
+      {
+        title: "Termo - 22/07/2024 - TF 09/2024 - ACIPP FMDCA",
         link: PDF9,
+        newTab: true,
+      },
+      {
+        title: "Municipal FMDCA TF 9/2024 – Despesas Anual 2024",
+        link: PDF21,
+        newTab: true,
+      },
+      {
+        title: "Municipal RP 10 – TF 09/2024",
+        link: PDF23,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "TF 13/2023 - TA 50/2023",
+    documents: [
+      {
+        title: "TA 50/23 ao TF 13/23 - ACIPP",
+        link: PDF7,
+        newTab: true,
+      },
+      {
+        title: "Municipal FMDCA TF 13/2023 – TA 50/2023 – Despesas Anual 2024",
+        link: PDF20,
         newTab: true,
       },
     ],
