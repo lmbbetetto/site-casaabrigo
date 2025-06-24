f00d46e2ed8f18a497d917117073c6e6c12a280f
Leonardo <leonardo.batista@startyouapp.com.br>
2025-06-23 22:58:28 -0300

    Feature: new documentes on 2024

--- src/pages/transparencia/mock/mock-2024.ts
+++ src/pages/transparencia/mock/mock-2024.ts
@@ -16,11 +16,12 @@ import PDF17 from "../../../assets/transparenciaaa/2024/novos/FEDERAL RELAÇÃO 
 import PDF18 from "../../../assets/transparenciaaa/2024/novos/FEDERAL RP 10 TF 11 2022 TA 6 24.pdf";
 import PDF19 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL EMENDAS IMPOSITIVAS TF 6 2024 DESPESAS ANUAL 2024.crdownload.pdf";
 import PDF20 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL FMDCA TF 13 2023 TA 50 2023 DESPESAS ANUAL 2024.crdownload.pdf";
-// import PDF21 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL FMDCA TF 9 2024 DESPESAS ANUAL 2024.crdownload.pdf";
 import PDF22 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL RP 10 TF 06 2024.pdf";
 import PDF23 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL RP 10 TF 09 2024.pdf";
 import PDF24 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL RP 10 TF 12 2022 TA 03 24.pdf";
 import PDF25 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL TF 12 2022 TA 3 2024 DESPESAS ANUAL 2024.crdownload.pdf";
+import PDF26 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL CONCILIAÇÃO BANCARIA TF 09 2024.pdf";
+import PDF27 from "../../../assets/transparenciaaa/2024/novos/MUNICIPAL EXTRATO TF 09 2024-3.pdf";
 
 export const documents2024 = [
   {
@@ -133,14 +134,19 @@ export const documents2024 = [
         link: PDF9,
         newTab: true,
       },
-      // {
-      //   title: "Municipal FMDCA TF 9/2024 – Despesas Anual 2024",
-      //   link: PDF21,
-      //   newTab: true,
-      // },
       {
         title: "Municipal RP 10 – TF 09/2024",
         link: PDF23,
+        newTab: true,
+      },
+      {
+        title: "Extrato Municipal – TF 09/2024",
+        link: PDF27,
+        newTab: true,
+      },
+      {
+        title: "Conciliação Bancária Municipal – TF 09/2024",
+        link: PDF26,
         newTab: true,
       },
     ],
