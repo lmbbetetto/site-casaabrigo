6ce396d7d56a8eeca5dd511fd864464c5ff278ce
Leonardo Betetto <lmbbetetto@gmail.com>
2026-03-24 14:06:26 -0300

    Fix: fix the names and files dont open

--- src/pages/transparencia/mock/mock-2025.ts
+++ src/pages/transparencia/mock/mock-2025.ts
@@ -1,20 +1,20 @@
 import PDF1 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 12-2025/Plano_0043502_2025.02.10_TA_00122025_TF_00132022_PP_335_ACIPP_E_R__55.345_81_PT.pdf";
 import PDF2 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 12-2025/Publicacao_0043729_2025.02.11_TA_00122025_publicacao.pdf"
-import PDF3 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 12-2025/Termo_0043499_2025.02.10_TA_00122025_TF_00132022_PP_335_ACIPP_E_R__55.345_81_assinado.pdf"
-
+import PDF3 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 12-2025/Termo_0043499_novo.pdf"
 import PDF4 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 32-2025/PlanoPP0955002255.pdf";
+import PDF23 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA_37_25_AO_TF_13_22_ACIPP.pdf";
 
 import PDF5 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 62-2024/Plano_0035773_2024.11.26_Plano_de_Trabalho_R__2.700_00.pdf"
 import PDF6 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 62-2024/Publicacao_0035766_2024.12.19_TA_00622024_publicacao_DOEM.pdf";
-import PDF7 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 62-2024/Termo_0035769_2024.12.19_TA_00622024_TF_00132022_PP_299_ACIPP_estadual_R__2.700_00_assinado.pdf"
+import PDF7 from "../../../assets/transparenciaaa/2025/2025/Estadual/TA 62-2024/Termo_0035769.pdf"
 
 import PDF8 from "../../../assets/transparenciaaa/2025/2025/Federal/TA 11-2025/Plano_0043482_2025.02.10_TA_00112025_TF_00112022_PP_336_ACIPP_F_R__30.000_00_PT.pdf";
 import PDF9 from "../../../assets/transparenciaaa/2025/2025/Federal/TA 11-2025/Publicacao_0043724_2025.02.11_TA_00112025_publicacao.pdf";
-import PDF10 from "../../../assets/transparenciaaa/2025/2025/Federal/TA 11-2025/Termo_0043480_2025.02.10_TA_00112025_TF_00112022_PP_336_ACIPP_F_R__30.000_00_assinado.pdf";
+import PDF10 from "../../../assets/transparenciaaa/2025/2025/Federal/TA 11-2025/Termo_0043480.pdf";
 
 import PDF11 from "../../../assets/transparenciaaa/2025/2025/Municial/TA 13-2025/Plano_0043532_2025.02.10_TA_00132025_TF_00122022_PP_334_ACIPP_M_R__136.962_72_PT.pdf";
 import PDF12 from "../../../assets/transparenciaaa/2025/2025/Municial/TA 13-2025/Publicacao_0043734_2025.02.11_TA_00132025_publicacao.pdf";
-import PDF13 from "../../../assets/transparenciaaa/2025/2025/Municial/TA 13-2025/Termo_0043529_2025.02.10_TA_00132025_TF_00122022_PP_334_ACIPP_M_R__136.962_72_assinado.pdf";
+import PDF13 from "../../../assets/transparenciaaa/2025/2025/Municial/TA 13-2025/Termo_0043529_2025.02.10_TA_00132025_TF_00122022_PP_334_ACIPP_M_R__136.962_72_assinado_novo.pdf";
 
 import PDF14 from "../../../assets/transparenciaaa/2025/2025/FMDCA/PT 34-2025/Plano_0100109_Plano_de_Trabalho_421__R__48.400_00_04_08_2025.pdf";
 import PDF15 from "../../../assets/transparenciaaa/2025/2025/FMDCA/PT 34-2025/Publicacao_0100208_2025.09.10_SEI_0099443_Autorizacao_Prefeito_publicada.pdf";
@@ -22,54 +22,49 @@ import PDF16 from "../../../assets/transparenciaaa/2025/2025/FMDCA/PT 34-2025/SE
 
 import PDF17 from "../../../assets/transparenciaaa/2025/2025/FMDCA/TF 13-2025/Extrato_0125485_Extrato_Publicacao_TF_00132025_11_12_2025.pdf";
 import PDF18 from "../../../assets/transparenciaaa/2025/2025/FMDCA/TF 13-2025/Extrato_0125510_Extrato_Republicacao_TF_00132025_12_12_2025.pdf";
-import PDF19 from "../../../assets/transparenciaaa/2025/2025/FMDCA/TF 13-2025/Termo_0124963_TF_00132025_PP_421_ACIPP_FMDCA_R__48.400_00_assinado.pdf";
+import PDF19 from "../../../assets/transparenciaaa/2025/2025/FMDCA/TF 13-2025/Termo_0124963_TF_00132025_PP_421_ACIPP_FMDCA_R__48.400_00_assinado_novo.pdf";
 
 import PDF20 from "../../../assets/transparenciaaa/2025/2025/emenda vereadores/TF 02-2025/Plano_0070777_2025.06.03_TF_00022025_PP_371_ACIPP_R__65.500_00_PT.pdf";
 import PDF21 from "../../../assets/transparenciaaa/2025/2025/emenda vereadores/TF 02-2025/Publicacao_0071397_2025.06.05_TF_00022025_PP_371_ACIPP_R__65.500_00_extrato_DOEM.pdf";
-import PDF22 from "../../../assets/transparenciaaa/2025/2025/emenda vereadores/TF 02-2025/Termo_0070772_2025.06.04_TF_00022025_PP_371_ACIPP_R__65.500_00_assinado.pdf";
+import PDF22 from "../../../assets/transparenciaaa/2025/2025/emenda vereadores/TF 02-2025/Termo_0070772_2025.06.04_TF_00022025_PP_371_ACIPP_R__65.500_00_assinado_novo.pdf";
 
 export const documents2025 = [
   {
-    name: "TA 12/2025",
+    name: "Estadual - TF 13/2022 - TA 12/2025 - TA 32/2025 - TA 37/2025 - TA 62/2024",
     documents: [
+      {
+        title: "Plano de Trabalho – TA 12/2025",
+        link: PDF3,
+        newTab: true,
+      },      
+      {
+        title: "Publicação DOEM – TA 12/2025",
+        link: PDF2,
+        newTab: true,
+      },
+      {
+        title: "Plano de Trabalho – TA 62/2024",
+        link: PDF5,
+        newTab: true,
+      },
+      {
+        title: "Publicação DOEM – TA 62/2024",
+        link: PDF6,
+        newTab: true,
+      },
       {
         title: "TA 12/2025 – Termo Aditivo ao TF 13/2022",
         link: PDF1,
         newTab: true,
       },
       {
-        title: "Publicação DOEM – TA 12/2025 (TF 13/2022)",
-        link: PDF2,
-        newTab: true,
-      },
-      {
-        title: "Plano de Trabalho – TA 12/2025 (TF 13/2022)",
-        link: PDF3,
-        newTab: true,
-      },
-    ],
-  },
-  {
-    name: "TA 32/2025",
-    documents: [
-      {
         title: "TA 32/2025 – Termo Aditivo ao TF 13/2022",
         link: PDF4,
         newTab: true,
       },
-    ],
-  },
-  {
-    name: "TA 62/2024",
-    documents: [
       {
-        title: "Plano de Trabalho – TA 62/2024 (TF 13/2022)",
-        link: PDF5,
-        newTab: true,
-      },
-      {
-        title: "Publicação DOEM – TA 62/2024 (TF 13/2022)",
-        link: PDF6,
+        title: "TA 37/2025 – Termo Aditivo ao TF 13/2022",
+        link: PDF23,
         newTab: true,
       },
       {
@@ -80,41 +75,81 @@ export const documents2025 = [
     ],
   },
   {
-    name: "TA 11/2025",
+    name: "Federal - TF 11/2022 - TA 11/2025",
     documents: [
       {
-        title: "Plano de Trabalho – TA 11/2025 (TF 11/2022)",
+        title: "Plano de Trabalho – TA 11/2025",
         link: PDF8,
         newTab: true,
       },
       {
-        title: "Publicação DOEM – TA 11/2025 (TF 11/2022)",
+        title: "Publicação DOEM – TA 11/2025",
         link: PDF9,
         newTab: true,
       },
       {
-        title: "TA 011/2025 – Termo Aditivo ao TF 11/2022",
+        title: "TA 11/2025 – Termo Aditivo ao TF 11/2022",
         link: PDF10,
         newTab: true,
       },
     ],
   },
   {
-    name: "TA 13/2025",
+    name: "Municial - TF 12/2022 - TA 13/2025",
     documents: [
       {
-        title: "Plano de Trabalho – TA 13/2025 (TF 12/2022)",
+        title: "Plano de Trabalho – TA 13/2025",
         link: PDF11,
         newTab: true,
       },
       {
-        title: "Publicação DOEM – TA 13/2025 (TF 12/2022)",
+        title: "Publicação DOEM – TA 13/2025",
         link: PDF12,
         newTab: true,
       },
       {
         title: "TA 13/2025 – Termo Aditivo ao TF 12/2022",
         link: PDF13,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "FMDCA - TF 13/2025",
+    documents: [
+      {
+        title: "TF 13/2025 – Termo de Fomento",
+        link: PDF19,
+        newTab: true,
+      },
+      {
+        title: "Publicação DOEM – TF 13/2025",
+        link: PDF17,
+        newTab: true,
+      },
+      {
+        title: "Republicação DOEM – TF 13/2025",
+        link: PDF18,
+        newTab: true,
+      },
+    ],
+  },
+  {
+    name: "Emendas impositivas vereadores - TF 02/2025",
+    documents: [
+      {
+        title: "TF 02/2025 – Termo de Fomento",
+        link: PDF22,
+        newTab: true,
+      },
+      {
+        title: "Plano de Trabalho – TF 02/2025",
+        link: PDF20,
+        newTab: true,
+      },
+      {
+        title: "Publicação DOEM – TF 02/2025",
+        link: PDF21,
         newTab: true,
       },
     ],
@@ -135,46 +170,6 @@ export const documents2025 = [
       {
         title: "Autorização/Justificativa – Termo de Fomento (Plano 34/2025)",
         link: PDF16,
-        newTab: true,
-      },
-    ],
-  },
-  {
-    name: "TF 13/2025",
-    documents: [
-      {
-        title: "Publicação DOEM – TF 13/2025",
-        link: PDF17,
-        newTab: true,
-      },
-      {
-        title: "Republicação DOEM – TF 13/2025",
-        link: PDF18,
-        newTab: true,
-      },
-      {
-        title: "TF 13/2025 – Termo de Fomento",
-        link: PDF19,
-        newTab: true,
-      },
-    ],
-  },
-  {
-    name: "TF 02/2025",
-    documents: [
-      {
-        title: "Plano de Trabalho – TF 2/2025",
-        link: PDF20,
-        newTab: true,
-      },
-      {
-        title: "Publicação DOEM – TF 2/2025",
-        link: PDF21,
-        newTab: true,
-      },
-      {
-        title: "TF 2/2025 – Termo de Foment",
-        link: PDF22,
         newTab: true,
       },
     ],
